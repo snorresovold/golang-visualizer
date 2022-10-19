3703c6250684c4fbcb4dd69f6579b020c5d270b9
snorresovold <snorresovold@gmail.com>
2022-10-19 13:03:14 +0200

    updates

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/snorresovold/golang-visualizer
+
+go 1.18
+
+require github.com/fogleman/gg v1.3.0
+
+require (
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20221017200508-ffcb3fe7d1bf // indirect
+)
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "github.com/fogleman/gg"
+
+func main() {
+	const S = 1024
+	dc := gg.NewContext(S, S)
+	dc.SetRGBA(0, 0, 0, 0.1)
+	for i := 0; i < 360; i += 15 {
+		dc.Push()
+		dc.RotateAbout(gg.Radians(float64(i)), S/2, S/2)
+		dc.DrawEllipse(S/2, S/2, S*7/16, S/8)
+		dc.Fill()
+		dc.Pop()
+	}
+	dc.SavePNG("out.png")
+}
