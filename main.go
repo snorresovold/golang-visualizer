b93605e412e3739bfdc78c1da452384566aa8446
snorresovold <snorresovold@gmail.com>
2022-10-30 01:49:07 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 type cell struct {
@@ -15,6 +16,23 @@ type cellList struct {
 	cells []cell
 }
 
+func (cl *cellList) AddCell(c cell) []cell {
+	cl.cells = append(cl.cells, c)
+	return cl.cells
+}
+
 func main() {
-	fmt.Println("sus")
+	cl := cellList{}
+	c := cell{false, 1, 2, 3}
+	cl.AddCell(c)
+	c2 := cell{true, 100, 2123, 3342}
+	cl.AddCell(c2)
+	fmt.Println(cl.cells)
+
+	swapC := reflect.Swapper(cl.cells)
+	cl.cells[0].switching, cl.cells[1].switching = true, true
+	swapC(0, 1)
+	fmt.Println(cl.cells)
+	cl.cells[0].switching, cl.cells[1].switching = false, false
+	fmt.Println(cl.cells)
 }
